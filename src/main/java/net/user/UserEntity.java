2c71c834e21bc1cd68f42e0484a91f1bc0ad9670
SatoYusuke0228 <62002722+SatoYusuke0228@users.noreply.github.com>
2020-11-07 21:03:05 +0900

    USERテーブルのコメントアウトを編集

--- src/main/java/net/user/UserEntity.java
+++ src/main/java/net/user/UserEntity.java
@@ -2,25 +2,30 @@ package net.user;
 
 import java.sql.Timestamp;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.transaction.Transactional;
 
 import lombok.Data;
 import net.common.GenderConverter;
-import net.user.registration.UserRegistration;
+import net.user.thumbnail.ThumbnailEntity;
 
 @Entity
 @Table(name = "USER_TABLE")
 @Data
 @Transactional
 public class UserEntity {
-//implements UserDetails { //userDetailsのinterfaceを使うかどうか
+	//implements UserDetails { //userDetailsのinterfaceを使うかどうか
 
 	@Id
 	@Column(name = "ID", unique = true)
@@ -44,6 +49,9 @@ public class UserEntity {
 	@Column(name = "MAIL_VERIFIED")
 	private boolean mailVerified;
 
+	//@Column(name = "SHOW_FLAG")
+	//private boolean showFlag;
+
 	@Column(name = "FIRST_NAME", nullable = false)
 	private String firstName;
 
@@ -60,6 +68,10 @@ public class UserEntity {
 	@Column(name = "MAIL", nullable = false, unique = true)
 	private String mail;
 
+	//登録地域
+	//@Column(name = "REGISTRATION_AREA")
+	//private String registrationArea;
+
 	@Column(name = "REGISTRATION_DATE", nullable = false)
 	private Timestamp registrationDate;
 
@@ -75,7 +87,7 @@ public class UserEntity {
 	 * @param mail
 	 * @param registrationDate
 	 */
-	public UserEntity(UserRegistration userRegistration) {
+	public UserEntity(User userRegistration) {
 
 		if (userRegistration.getPassword().equals(userRegistration.getPassword_auth())) {
 			this.password = userRegistration.getPassword();
@@ -84,11 +96,13 @@ public class UserEntity {
 		this.userType = userRegistration.getUserType();
 		this.activation = true;
 		this.mailVerified = false;
+//		this.showFlag = false;
 		this.firstName = userRegistration.getFirstName();
 		this.lastName = userRegistration.getLastName();
 		this.gender = userRegistration.getGender();
 		this.tell = userRegistration.getTell();
 		this.mail = userRegistration.getMail();
+//		this.registrationArea = userRegistration.getRegistrationArea();
 		this.registrationDate = new Timestamp(System.currentTimeMillis());
 	}
 
@@ -96,16 +110,15 @@ public class UserEntity {
 		super();
 	}
 
-	//	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-	//	@JoinTable(name = "THUMBNAIL", joinColumns = {
-	//			@JoinColumn(name = "ID", referencedColumnName = "ID") }, inverseJoinColumns = {
-	//					@JoinColumn(name = "ID", referencedColumnName = "ID") })
-	//	private ThumbnailEntity thumbnailEntity;
+	@OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "THUMBNAIL_TABLE", joinColumns = {
+			@JoinColumn(name = "ID", referencedColumnName = "ID") }, inverseJoinColumns = {
+					@JoinColumn(name = "ID", referencedColumnName = "ID") })
+	private ThumbnailEntity thumbnailEntity;
 
 	//	@Override
 	//	public Collection<? extends GrantedAuthority> getAuthorities() {
-	//		// TODO 自動生成されたメソッド・スタブ
-	//		return null;
+	//		return ;
 	//	}
 	//
 	//	@Override
@@ -114,26 +127,22 @@ public class UserEntity {
 	//	}
 	//
 	//	@Override
-	//	public boolean isAccountNonExpired() {
-	//		// TODO 自動生成されたメソッド・スタブ
+	//	public boolean isAccountNonExpired() { //アカウントの有効期限が切れていない
 	//		return false;
 	//	}
 	//
 	//	@Override
-	//	public boolean isAccountNonLocked() {
-	//		// TODO 自動生成されたメソッド・スタブ
+	//	public boolean isAccountNonLocked() { //アカウントがロックされていない
 	//		return false;
 	//	}
 	//
 	//	@Override
-	//	public boolean isCredentialsNonExpired() {
-	//		// TODO 自動生成されたメソッド・スタブ
+	//	public boolean isCredentialsNonExpired() { //資格情報の有効期限が切れていない
 	//		return false;
 	//	}
 	//
 	//	@Override
-	//	public boolean isEnabled() {
-	//		// TODO 自動生成されたメソッド・スタブ
-	//		return false;
+	//	public boolean isEnabled() { //有効
+	//		return this.activation;
 	//	}
 }
