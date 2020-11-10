0ad4b8d30b55606de3e805c06960014778780b19
SatoYusuke0228 <62002722+SatoYusuke0228@users.noreply.github.com>
2020-11-10 21:42:54 +0900

    ページアクセス権限の変更

--- src/main/java/net/admin/WebSecurityConfig.java
+++ src/main/java/net/admin/WebSecurityConfig.java
@@ -1,7 +1,9 @@
 package net.admin;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -9,11 +11,16 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	UserDetailsService userDetailsService;
 
 	/**
 	 * 「css」や「image」配下のファイルにアクセスする際に
@@ -23,32 +30,55 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	public void configure(WebSecurity web) {
-		web.ignoring().antMatchers(
-				"/image/**",
-				"/css/**",
-				"/js/**");
+
+		web.ignoring()
+				.antMatchers("/image/**", "/css/**", "/js/**");
 	}
 
 	/**
 	 * トップページや商品詳細ページはログインしなくても見せたいページなので、
 	 *  これらのページは引き続き誰でも自由にアクセスすることを可能にする
 	 *
-	 * @param  .antMatchers("/admin/**")
-	 *         .authenticated()
-	 *         「/adminのパスのページは全て認証が必要」と設定
+	 * @param  <code>.antMatchers("/admin/**").authenticated()</code>
+	 *         「"/admin"のパスのページは全て認証が必要」と設定
 	 *
-	 * @param  .formLogin()
-	 *         .permitAll();
+	 * @param  .formLogin().permitAll();
 	 *          ログインページをだれでも閲覧できるように設定
 	 *
+	 * @param  .loginProcessingUrl()
+	 *         認証処理に移るためのURLを指定
+	 *
+	 * @param  .defaultSuccessUrl()
+	 *         ログインに成功した場合に移動するページのURL
 	 */
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
+
 		http.authorizeRequests()
-				.antMatchers("/admin/**")
-				.authenticated()
-				.and()
-				.formLogin()
+				//.anyRequest().authenticated()
+				//誰でもアクセス可能なページ
+				.antMatchers("/", "/index").permitAll()
+				.antMatchers("/registration/**").permitAll()
+				.anyRequest().authenticated();
+		//アクセスに権限が必要なぺージ
+		//				.antMatchers("/login-success").authenticated()
+		//				.antMatchers("/admin/**").authenticated()
+		//				.antMatchers("/user/**").authenticated();
+
+		//ログインページを指定。
+		//ログインページへのアクセスは全員許可する。
+		http.formLogin()
+				.loginPage("/login")
+				.loginProcessingUrl("/authenticate")
+				.usernameParameter("userName") //HTML[name="userName"]と同一
+				.passwordParameter("password") //HTML[name="password"]と同一
+				.failureUrl("/login?error")
+				.defaultSuccessUrl("/login-success")
+				.permitAll()
+			.and()
+				.logout()
+				.logoutUrl("/logout")
+				.logoutSuccessUrl("/login?logout")
 				.permitAll();
 	}
 
@@ -68,5 +98,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Autowired
+	void configureAuthenticationManager(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService)
+				.passwordEncoder(passwordEncoder());
 	}
 }
